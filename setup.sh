ee5f77935fcbc7ebc73b3ec5e68c117ef735a351
isolovey-robarts <12663941+isolovey-robarts@users.noreply.github.com>
2019-10-03 16:34:09 -0400

    WIP: Make the default cfg file more general (#9)
    * (wip) Make the default cfg file more general
    
    * fix for multiple groups; sensible defaults for license/grad file
    
    * Add setup.sh script, supply profile as argument
    
    * changed to singularity 3.4, use path to setup.sh to find init path
    
    * loading singularity module first 
    
    so we can override $SINGULARITY_TMPDIR to use /tmp (system defaults to scratch)..
    
    * added SINGULARITY_TMPDIR=/tmp
    
    * added SINGULARITY_TMPDIR to khanlab cfg

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+execpath=`dirname $0`
+execpath=`realpath $execpath`
+
+
+if [ $# -gt 0 ]; then
+  cfg_profile=_$1
+else
+  cfg_profile=
+fi
+
+echo "module load singularity/3.4" >> ~/.bash_profile
+echo "cfg_profile=${cfg_profile}" >> ~/.bash_profile
+echo "source $execpath/00_init.sh" >> ~/.bash_profile

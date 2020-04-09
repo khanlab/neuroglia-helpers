4ac749c2383ccfe9033761b384d393e7500bb4a8
akhanf <alik@robarts.ca>
2020-04-09 14:49:19 -0400

    updates to snakemake_slurm wrapper, now handles grouped jobs, and enables modules/singularity by default

--- etc/snakemake_slurm_submit.py
+++ etc/snakemake_slurm_submit.py
@@ -14,35 +14,60 @@ dependencies = set(sys.argv[1:-1])
 # parse the job script for the job properties that are encoded by snakemake within
 job_properties = read_job_properties(jobscript)
 
+
+
 # get account from CC_COMPUTE_ALLOC, else supply default account
-account = os.getenv('CC_COMPUTE_ALLOC', default='rrg-akhanf')
+account = os.getenv('CC_COMPUTE_ALLOC', default='ctb-akhanf')
 
-#if len(job_properties['log']) > 0 :
-#    log = job_properties['log'][0]
-#else:
+   
+
+if job_properties["type"]=='single':
     
-log = os.path.join('logs','slurm','slurm_%j_{}_{}.out'.format(job_properties['jobid'],job_properties['rule']))
+    #this is a single rule, so set the resource request based on the rule itself
+    # or to single rule defaults (1 core, 1hr, 4gb)
+    job_name = job_properties['rule']
 
-log = os.path.realpath(log)
+    #get values and set defaults
+    if 'time' in job_properties["resources"].keys():
+        time = job_properties["resources"]["time"]
+    else:  
+        time = 60
+
+    if 'mem_mb' in job_properties["resources"].keys():
+        mem_mb = job_properties["resources"]["mem_mb"]
+    else:  
+        mem_mb = 4000
+
+    #for single job, threads already set to 1 by default
+    threads = job_properties["threads"]
+
+
+elif job_properties["type"]=='group':
+
+    #for a grouped rule, we request an entire node in order to maximize throughput:
+    job_name = job_properties['groupid']
+
+    # Maybe look into later: add some checks to 
+    #  1) set a reasonable walltime (less than 24hrs) based on 1hr 1core jobs
+    #  2) check when there may be too many jobs in the group
+    # if 2), could always try to resubmit with larger job?  
+    
+    time = 60*24
+    mem_mb = 128000
+    threads = 32
+
+else:
+    raise NotImplementedError(f"Don't know what to do with job_properties['type']=={job_properties['type']}")
+
+
+
+log = os.path.realpath(os.path.join('logs','slurm',f'slurm_%j_{job_name}.out'))
 
 #create the log directory (slurm fails if doesn't exist)
 log_dir = os.path.dirname(log)
 if not os.path.exists(os.path.dirname(log)):
     os.makedirs(log_dir)
 
-#get values and set defaults
-if 'time' in job_properties["resources"].keys():
-    time = job_properties["resources"]["time"]
-else:  
-    time = 60
-
-if 'mem_mb' in job_properties["resources"].keys():
-    mem_mb = job_properties["resources"]["mem_mb"]
-else:  
-    mem_mb = 4000
-
-#threads already set to 1 by default
-threads = job_properties["threads"]
 
 
 # collect all command-line options in an array
